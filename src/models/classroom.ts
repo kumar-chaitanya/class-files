acf56c6911e175bac63277fa9b8a88ba333dd112
Chaitanya Kumar <chaitanya.kumar@upfronthealthcare.com>
2024-03-21 15:38:01 +0530

    updated user and classroom model

--- src/models/classroom.ts
+++ src/models/classroom.ts
@@ -1,21 +1,39 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/connection';
-import User from './user';
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
 
-const Classroom = sequelize.define('classroom', {
-  id: {
-    type: DataTypes.STRING(36),
-    autoIncrement: false,
+@Table({
+  timestamps: true
+})
+class Classroom extends Model<Classroom> {
+  @Column({
+    type: DataType.STRING(36),
+    defaultValue: DataType.UUIDV4,
+    primaryKey: true,
     allowNull: false,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-});
+    autoIncrement: false
+  })
+  id!: string;
 
-Classroom.belongsTo(User, { foreignKey: { name: 'teacherId', allowNull: false }, as: 'teacher' });
+  @Column({
+    type: DataType.STRING,
+    allowNull: false
+  })
+  name!: string;
+};
+
+// const Classroom = sequelize.define('classroom', {
+//   id: {
+//     type: DataTypes.STRING(36),
+//     autoIncrement: false,
+//     allowNull: false,
+//     defaultValue: DataTypes.UUIDV4,
+//     primaryKey: true
+//   },
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   }
+// });
+
+// Classroom.belongsTo(User, { foreignKey: { name: 'teacherId', allowNull: false }, as: 'teacher' });
 
 export default Classroom;
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,27 +1,60 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/connection';
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+import { UserRole } from '../types/custom.types';
 
-const User = sequelize.define('user', {
-  id: {
-    type: DataTypes.STRING(36),
-    autoIncrement: false,
+@Table({
+  timestamps: true
+})
+class User extends Model<User> {
+  @Column({
+    type: DataType.STRING(36),
+    defaultValue: DataType.UUIDV4,
+    primaryKey: true,
     allowNull: false,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true
-  },
-  username: {
-    type: DataTypes.STRING,
+    autoIncrement: false
+  })
+  id!: string;
+  
+  @Column({
+    type: DataType.STRING,
     unique: true,
     allowNull: false
-  },
-  password: {
-    type: DataTypes.STRING,
+  })
+  username!: string;
+
+  @Column({
+    type: DataType.STRING,
     allowNull: false
-  },
-  role: {
-    type: DataTypes.ENUM('teacher', 'student'),
+  })
+  password!: string;
+
+  @Column({
+    type: DataType.ENUM('teacher', 'student'),
     allowNull: false
-  }
-});
+  })
+  role!: UserRole
+};
+
+// const User = sequelize.define('user', {
+//   id: {
+//     type: DataTypes.STRING(36),
+//     autoIncrement: false,
+//     allowNull: false,
+//     defaultValue: DataTypes.UUIDV4,
+//     primaryKey: true
+//   },
+//   username: {
+//     type: DataTypes.STRING,
+//     unique: true,
+//     allowNull: false
+//   },
+//   password: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   role: {
+//     type: DataTypes.ENUM('teacher', 'student'),
+//     allowNull: false
+//   }
+// });
 
 export default User;
