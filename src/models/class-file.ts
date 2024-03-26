5b21301d6b572314d499a313ef1676198edef5f4
Chaitanya Kumar <chaitanya.kumar@upfronthealthcare.com>
2024-03-26 14:41:57 +0530

    updated model file to class-file

---
+++ src/models/class-file.ts
@@ -0,0 +1,56 @@
+import { Table, Column, Model, DataType, ForeignKey } from 'sequelize-typescript';
+import User from './user';
+import Classroom from './classroom';
+
+@Table({
+    timestamps: true
+})
+class ClassFile extends Model<ClassFile> {
+    @Column({
+        type: DataType.STRING(36),
+        defaultValue: DataType.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: false
+    })
+    id!: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    name!: string;
+
+    @Column({
+        type: DataType.TEXT
+    })
+    description!: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    type!: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    path!: string;
+
+    @ForeignKey(() => User)
+    @Column({
+        type: DataType.STRING(36),
+        allowNull: false
+    })
+    uploadedBy!: string;
+
+    @ForeignKey(() => Classroom)
+    @Column({
+        type: DataType.STRING(36),
+        allowNull: false
+    })
+    classroomId!: string;
+};
+
+export default ClassFile;
