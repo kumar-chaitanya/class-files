69287aa7ce6f86e8552681ba0ffdd16f38f8fa18
Chaitanya Kumar <chaitanya.kumar@upfronthealthcare.com>
2024-04-02 17:40:01 +0530

    updated database config to use env varaibles

--- src/config/create-database.ts
+++ src/config/create-database.ts
@@ -1,15 +1,14 @@
 import mysql from 'mysql2/promise';
-import Config from './config';
 
 async function createDatabase(): Promise<void> {
     try {
         const connection = await mysql.createConnection({
-            host: Config.databaseHost,
-            user: Config.databaseUsername,
-            password: Config.databasePassword
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD
         });
 
-        await connection.query(`CREATE DATABASE IF NOT EXISTS ${Config.databaseName}`);
+        await connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`);
         console.log('Database created successfully');
     } catch (error) {
         console.error('Error creating database:', error);
